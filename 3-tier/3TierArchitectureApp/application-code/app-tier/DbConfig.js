360b01f62b70d9640d5f882951d9b47cae670e6d
Sadia Anjum Shaik Hari <sadiaanjumsh@gmail.com>
2025-05-09 22:23:17 +0530

    Add files via upload

---
+++ 3-tier/3TierArchitectureApp/application-code/app-tier/DbConfig.js
@@ -0,0 +1,6 @@
+module.exports = Object.freeze({
+    DB_HOST : 'database-1.cl0uegueua0f.ap-south-1.rds.amazonaws.com',
+    DB_USER : 'admin',
+    DB_PWD : 'Doodu146Peera',
+    DB_DATABASE : 'webappdb'
+});
