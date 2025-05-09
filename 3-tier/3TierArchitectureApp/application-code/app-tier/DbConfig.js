7eb184bf22fb7c088262cc4832fc208917499c52
Sadia Anjum Shaik Hari <sadiaanjumsh@gmail.com>
2025-05-09 22:24:54 +0530

    Delete 3-tier/3TierArchitectureApp directory

--- 3-tier/3TierArchitectureApp/application-code/app-tier/DbConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = Object.freeze({
-    DB_HOST : 'database-1.cl0uegueua0f.ap-south-1.rds.amazonaws.com',
-    DB_USER : 'admin',
-    DB_PWD : 'Doodu146Peera',
-    DB_DATABASE : 'webappdb'
-});
